890d7e009c20a5ac89423289c41060d494f249df
Alex <areseye@163.com>
2018-08-19 18:06:13 +0800

    ReadMe

---
+++ news-crawler/ReadMe.md
@@ -0,0 +1,9 @@
+# Crawling news based on url list
+
+## sh run_bbcCrawler.sh
+
+## Crontab command
+
+<code>
+    */20 * * * * sh /path/to/run_bbcCrawler.sh
+</code>
--- news-crawler/run_bbcCrawler.sh
+++ news-crawler/run_bbcCrawler.sh
@@ -1,1 +1,1 @@
-python3 /home/alexg/vol1/data/news/crawler/bbcCrawler.py /home/alexg/vol1/data/news/crawler/newsUrlList.txt /home/alexg/vol1/data/news/crawler/bbcData/ /home/alexg/vol1/data/news/crawler/RawHtml/ 5
+python3 /home/alexg/vol1/data/news/crawler/bbcCrawler.py /home/alexg/vol1/data/news/crawler/newsUrlList.txt /home/alexg/vol1/data/news/crawler/crawler-output/newsdata-2018-08-19/ /home/alexg/vol1/data/news/crawler/crawler-output/RawHtml/ 5
