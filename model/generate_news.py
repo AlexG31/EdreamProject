19b4b800deb6cbd656c486b48162bbf824100314
Alex <areseye@163.com>
2019-07-07 13:20:07 +0800

    generate fake news

--- model/generate_news.py
+++ model/generate_news.py
@@ -1,16 +1,61 @@
 import gpt_2_simple as gpt2
 import tensorflow as tf
 from tensorflow.core.protobuf import rewriter_config_pb2
+import os, sys, re
 model_name = "117M"
-training_data = "../data/training-news.txt"
+output_file = sys.argv[1]
+print('output file: {}'.format(output_file))
 print('Using cached model 117M')
-print('Using training data {}'.format(training_data))
 
+
+def scriptFilter(line):
+    nameSet = set(['he',
+        'she',
+        'who',
+        'say',
+        'said',
+        'says',
+        'tell',
+        'tells',
+        'my',
+        'me',
+        'his',
+        'her',
+        'them',
+        'they',
+        'ours',
+        'our',
+        'us',
+        'jack',
+        'rose'
+    ])
+    patterns = []
+    for n in nameSet:
+        pat = re.compile(r'\b{}\b'.format(n))
+        patterns.append(pat)
+    rows = line.split('\n')
+    results = []
+    for r in rows:
+        rl = r.lower()
+        for w in nameSet:
+            if pat.search(rl) != None:
+                results.append(r)
+                break
+    return results
+        
 
 print('-----generate-----')
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
-gpt2.generate(sess,
+results = gpt2.generate(sess,
 length=100,
-nsamples=20,
-temperature=0.7)
+nsamples=100,
+temperature=0.7,
+return_as_list=True)
+
+with open(output_file, 'w', encoding='utf8') as fout:
+    for line in results:
+        rows = scriptFilter(line)
+        for r in rows:
+            fout.write(r)
+            fout.write('\n')
