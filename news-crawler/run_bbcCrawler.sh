2f32040a81e0dbcb2cb2af7c83f0aeb6f0b7afee
Alex G <AresEyE@163.com>
2019-07-07 08:25:50 +0800

    code cleanup

--- news-crawler/run_bbcCrawler.sh
+++
@@ -1,4 +0,0 @@
-python3 \
-bbcCrawler.py \
-newsUrlList.txt \
-/home/alexg/vol1/data/news/crawler/crawler-output/RawHtml/ 1
--- news-crawler/run_chrome_crawler.sh
+++ news-crawler/run_chrome_crawler.sh
@@ -3,6 +3,9 @@ E_DREAM_ROOT='/media/alexg/vol1/github/EdreamProject'
 cd $E_DREAM_ROOT
 cd news-crawler
 dateFileName="`date -I`"
+dailyFolder="./work/$dateFileName"
+mkdir -p "$dailyFolder"
+chmod 777 "$dailyFolder"
 node crawlTask \
 --urlFile './newsUrlList.txt' \
 --outputFolder './work'
@@ -10,4 +13,5 @@ node crawlTask \
 echo "=====Let's Merge Them!====="
 python3 $E_DREAM_ROOT/util/merge_data_util.py \
 './work' \
-"./work/$dateFileName.merged.txt"
+"./work/$dateFileName.merged.txt" \
+> "$dateFileName.log"
