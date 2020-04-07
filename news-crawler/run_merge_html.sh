b0931e83ee8f5059358ec5093248fbbef7f6161e
Alex G <AresEyE@163.com>
2020-04-07 23:50:29 +0800

    separate merege html

--- news-crawler/run_merge_html.sh
+++ news-crawler/run_merge_html.sh
@@ -1,5 +1,3 @@
-#!/bin/bash -x
-#E_DREAM_ROOT='/media/alexg/vol1/github/EdreamProject'
 E_DREAM_ROOT='/home/alexg/github/EdreamProject'
 cd $E_DREAM_ROOT
 cd news-crawler
@@ -7,7 +5,8 @@ dateFileName="`date -I`"
 dailyFolder="./work/$dateFileName"
 mkdir -p "$dailyFolder"
 chmod 777 "$dailyFolder"
-node crawlTask \
---urlFile './newsUrlList.txt' \
---outputFolder './work' \
-> "./work/$dateFileName.log" 2>&1
+
+echo "=====Let's Merge Them!====="
+python3 $E_DREAM_ROOT/util/merge_data_util.py \
+'./work' \
+"./work/$dateFileName.merged.txt" 
